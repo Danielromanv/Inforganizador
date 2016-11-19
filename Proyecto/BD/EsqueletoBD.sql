dc01f77979f3909e15a3cc97c32e469cec3fb1ff
Umauro <felipe.vega.14@sansano.usm.cl>
2016-11-18 21:03:21 -0300

    Cambio nombre columna Nombre Curso e ID autoincremental

--- Proyecto/BD/EsqueletoBD.sql
+++ Proyecto/BD/EsqueletoBD.sql
@@ -51,8 +51,8 @@ DROP TABLE IF EXISTS `curso`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `curso` (
-  `ID_curso` int(10) NOT NULL,
-  `Nombre Curso` varchar(64) NOT NULL,
+  `ID_curso` int(10) NOT NULL AUTO_INCREMENT,
+  `Nombre_Curso` varchar(64) NOT NULL,
   PRIMARY KEY (`ID_curso`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
